10e0ca3507084e95ad8d6ab837c4e543870616e3
Diogo.Mic <okolimichael2015@outlook.com>
2024-02-01 16:48:05 +0100

    ec2 terraform script

---
+++ EC2.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "eu-west-2" // London region
+}
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-0c94855ba95c71c99" // Amazon Linux 2 AMI ID
+  instance_type = "t2.micro"
+  key_name      = "your_key_pair_name" // Replace with your key pair name
+
+  tags = {
+    Name = "webserver-instance"
+  }
+}
+resource "aws_vpc" "webserver" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "webserver_subnet" {
+  vpc_id     = aws_vpc.webserver.id
+  cidr_block = "10.0.0.0/24"
+}
+resource "aws_security_group" "webserver_security_group" {
+  name        = "webserver-security-group"
+  description = "Webserver security group"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = aws_vpc.webserver.id
+}
